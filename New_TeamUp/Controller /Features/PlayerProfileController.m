bd262f9b9b76aea812c809fd0542c60e3b872334
hunglb1985 <hunglb.ctv@gmail.com>
2018-05-31 17:31:38 +0700

    Add player profile data

--- New_TeamUp/Controller /Features/PlayerProfileController.m
+++ New_TeamUp/Controller /Features/PlayerProfileController.m
@@ -8,11 +8,74 @@
 
 #import "PlayerProfileController.h"
 
+NSString *const kName = @"name";
+NSString *const kNumber = @"number";
+NSString *const kButton = @"button";
+
 @interface PlayerProfileController ()
 
 @end
 
 @implementation PlayerProfileController
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self){
+        [self initializeForm];
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self){
+        [self initializeForm];
+    }
+    return self;
+}
+
+
+-(void)initializeForm
+{
+    XLFormDescriptor * form;
+    XLFormSectionDescriptor * section;
+    XLFormRowDescriptor * row;
+    
+    form = [XLFormDescriptor formDescriptor];
+    
+    section = [XLFormSectionDescriptor formSectionWithTitle:@"Please set information below to register"];
+    section.footerTitle = @"We can add more text here to infor user";
+    [form addFormSection:section];
+    
+    
+    // Name
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kName rowType:XLFormRowDescriptorTypeText title:@"Name:"];
+    row.required = YES;
+    [section addFormRow:row];
+    
+    // Number
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kNumber rowType:XLFormRowDescriptorTypePhone title:@"Phone number:"];
+    [row.cellConfigAtConfigure setObject:@"Required..." forKey:@"textField.placeholder"];
+    [row.cellConfigAtConfigure setObject:@(NSTextAlignmentRight) forKey:@"textField.textAlignment"];
+    row.required = YES;
+    [row addValidator:[XLFormRegexValidator formRegexValidatorWithMsg:@"Wrong number" regex:@"[0-9]{10,11}"]];
+    [section addFormRow:row];
+    
+    
+    section = [XLFormSectionDescriptor formSectionWithTitle:@""];
+    section.footerTitle = @"";
+    [form addFormSection:section];
+    
+    // Button
+    XLFormRowDescriptor * buttonRow = [XLFormRowDescriptor formRowDescriptorWithTag:kButton rowType:XLFormRowDescriptorTypeButton title:@"Register"];
+//    buttonRow.action.formSelector = @selector(didTouchButton:);
+    [section addFormRow:buttonRow];
+    
+    self.form = form;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
